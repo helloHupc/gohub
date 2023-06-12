38bc94cadb9dbdba14f6bf8454bce11463cdc318
hupengchen <hupengchen@jiayuan.com>
2023-06-12 17:29:14 +0800

    integrate gin

--- main.go
+++ main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello 世界!")
+	// new一个gin Engine实例
+	r := gin.New()
+
+	// 注册中间件
+	r.Use(gin.Logger(), gin.Recovery())
+
+	// 注册一个路由
+	r.GET("/", func(c *gin.Context) {
+
+		//json格式响应
+		c.JSON(http.StatusOK, gin.H{
+			"hello": "world!",
+		})
+	})
+
+	// 处理404请求
+	r.NoRoute(func(c *gin.Context) {
+		// 获取表头信息 Accept 信息
+		acceptString := c.Request.Header.Get("Accept")
+
+		if strings.Contains(acceptString, "text/html") {
+			//如果是HTML的话
+			c.String(http.StatusNotFound, "页面返回 404")
+		} else {
+			// 默认返回 JSON
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认URL和请求方法是否正确。",
+			})
+		}
+	})
+
+	// 运行服务
+	r.Run(":8000")
 }
