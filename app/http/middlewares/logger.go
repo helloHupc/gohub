2b770d9849cce88a2f634c804ed453ece5555807
hupengchen <hupengchen@jiayuan.com>
2023-07-06 18:42:32 +0800

    http middlewares-1

---
+++ app/http/middlewares/logger.go
@@ -0,0 +1,39 @@
+// Package middlewares 存放系统中间件
+package middlewares
+
+import (
+	"bytes"
+	"io"
+
+	"github.com/gin-gonic/gin"
+)
+
+type responseBodyWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (r responseBodyWriter) Write(b []byte) (int, error) {
+	r.body.Write(b)
+	return r.ResponseWriter.Write(b)
+}
+
+// Logger 记录请求日志
+func Looger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		// 获取 response 内容
+		w := &responseBodyWriter{body: &bytes.Buffer{}, ResponseWriter: c.Writer}
+		c.Writer = w
+
+		// 获取请求数据
+		var requestBody []byte
+		if c.Request.Body != nil {
+			// c.Request.Body 是一个 buffer 对象，只能读取一次
+			requestBody, _ = io.ReadAll(c.Request.Body)
+			// 读取后，重新赋值 c.Request.Body ，以供后续的其他操作
+			c.Request.Body = io.NopCloser(bytes.NewBuffer(requestBody))
+		}
+
+	}
+}
