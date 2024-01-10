7a32532a77528ac9f28e4d9c0b1fa227410a964c
hupengchen <hupengchen@rdc.gnway.com>
2024-01-10 20:11:05 +0800

    reset password by phone

---
+++ app/http/controllers/api/v1/auth/password_controller.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	v1 "gohub/app/http/controllers/api/v1"
+	"gohub/app/models/user"
+	"gohub/app/requests"
+	"gohub/pkg/response"
+)
+
+// 用户控制器
+type PasswordController struct {
+	v1.BaseAPIController
+}
+
+// 使用手机和验证码重置密码
+func (pc *PasswordController) RestByPhone(c *gin.Context) {
+
+	// 1.验证表单
+	request := requests.RestByPhoneRequest{}
+	if ok := requests.Validate(c, &request, requests.RestByPhone); !ok {
+		return
+	}
+
+	// 2.更新密码
+	userModel := user.GetByPhone(request.Phone)
+	if userModel.ID == 0 {
+		response.Abort404(c)
+	} else {
+		userModel.Password = request.Password
+		userModel.Save()
+
+		response.Success(c)
+	}
+
+}
