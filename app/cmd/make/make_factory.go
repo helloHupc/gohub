5dc24c0076bf4391789be59ca6d84358c44acb5e
hupengchen <hupengchen@rdc.gnway.com>
2024-01-14 14:28:44 +0800

    make factory command

---
+++ app/cmd/make/make_factory.go
@@ -0,0 +1,24 @@
+package make
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var CmdMakeFactory = &cobra.Command{
+	Use:   "factory",
+	Short: "Create model's factory file, example: make factory user",
+	Run:   runMakeFactory,
+	Args:  cobra.ExactArgs(1),
+}
+
+func runMakeFactory(cmd *cobra.Command, args []string) {
+	// 格式化模型名称
+	model := makeModelFromString(args[0])
+
+	// 拼接目标文件路径
+	filePath := fmt.Sprintf("database/factories/%s_factory.go", model.PackageName)
+
+	// 基于模板创建文件
+	createFileFromStub(filePath, "factory", model)
+}
