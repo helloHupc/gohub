742341c2032406bc04b22e20c8dc258c26150aca
hupengchen <hupengchen@rdc.gnway.com>
2024-01-15 15:07:33 +0800

    update topic

--- app/http/controllers/api/v1/topics_controller.go
+++ app/http/controllers/api/v1/topics_controller.go
@@ -1,12 +1,12 @@
 package v1
 
 import (
-    "gohub/app/models/topic"
-    "gohub/app/requests"
-    "gohub/pkg/auth"
-    "gohub/pkg/response"
+	"gohub/app/models/topic"
+	"gohub/app/requests"
+	"gohub/pkg/auth"
+	"gohub/pkg/response"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 type TopicsController struct {
@@ -45,5 +45,28 @@ func (ctrl *TopicsController) Store(c *gin.Context) {
 		response.Created(c, topicModel)
 	} else {
 		response.Abort500(c, "创建失败，请稍后尝试~")
+	}
+}
+
+func (ctrl *TopicsController) Update(c *gin.Context) {
+	topicModel := topic.Get(c.Param("id"))
+	if topicModel.ID == 0 {
+		response.Abort404(c)
+		return
+	}
+
+	request := requests.TopicRequest{}
+	if ok := requests.Validate(c, &request, requests.TopicSave); !ok {
+		return
+	}
+
+	topicModel.Title = request.Title
+	topicModel.Body = request.Body
+	topicModel.CategoryID = request.CategoryID
+	rowsAffected := topicModel.Save()
+	if rowsAffected > 0 {
+		response.Data(c, topicModel)
+	} else {
+		response.Abort500(c, "更新失败，请稍后尝试~")
 	}
 }
