63cc955c3554b312e2efbb712373f987b73cb17a
hupengchen <hupengchen@rdc.gnway.com>
2024-01-16 18:52:11 +0800

    user upload avatar

--- app/requests/requests.go
+++ app/requests/requests.go
@@ -41,3 +41,15 @@ func validate(data interface{}, rules govalidator.MapData, messages govalidator.
 	// 开始验证
 	return govalidator.New(opts).ValidateStruct()
 }
+
+func validateFile(c *gin.Context, data interface{}, rules govalidator.MapData, messages govalidator.MapData) map[string][]string {
+	opts := govalidator.Options{
+		Request:       c.Request,
+		Rules:         rules,
+		Messages:      messages,
+		TagIdentifier: "valid",
+	}
+
+	// 调用govalidator的Validate方法来验证文件
+	return govalidator.New(opts).Validate()
+}
